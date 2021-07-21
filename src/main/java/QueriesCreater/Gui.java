6ebb4b540d2886f7cf565742170e1bc3fc885073
mrprogre <rps_project@mail.ru>
2021-07-21 23:30:48 +0300

    upd_21_07_21_4

--- src/main/java/QueriesCreater/Gui.java
+++ src/main/java/QueriesCreater/Gui.java
@@ -3,7 +3,13 @@ package QueriesCreater;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.JTableHeader;
@@ -231,9 +237,7 @@ public class Gui extends JFrame {
 
         // выгрузка запросов в текстовый файл
         if (columns.length > 0) {
-            WriteToFile wr = new WriteToFile();
-            wr.write(new GetSqlQuery().getQueries(objectParameters, columns));
-            //System.out.println(sql.getQueries(columns));
+            write(new GetSqlQuery().getQueries(objectParameters, columns));
         }
     }
 
@@ -261,6 +265,24 @@ public class Gui extends JFrame {
                 }
             }
             return c;
+        }
+    }
+
+    static void write(String pSql) {
+        //Save file to
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("*.txt", "*.txt", "*.TXT", "*.*");
+        JFileChooser save_to = new JFileChooser();
+        save_to.setFileFilter(filter);
+        save_to.setCurrentDirectory(new File
+                (System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop"));
+        int ret = save_to.showDialog(null, "Save");
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(save_to.getSelectedFile() + ".txt"), StandardCharsets.UTF_8)) {
+                writer.write(pSql);
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
