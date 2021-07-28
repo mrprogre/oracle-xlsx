d3902f6f5dbc40ef2547d314136d9a7b7972c8a1
Dmitry <rps_project@mail.ru>
2021-07-28 17:13:00 +0300

    Update Gui.java
    upd_28_07_21_1

--- src/main/java/QueriesCreater/Gui.java
+++ src/main/java/QueriesCreater/Gui.java
@@ -3,10 +3,7 @@ package QueriesCreater;
 import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.nio.charset.StandardCharsets;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
@@ -32,8 +29,8 @@ public class Gui extends JFrame {
         this.setTitle("PL/SQL: excel export");
         this.setFont(new Font("Tahoma", Font.PLAIN, 14));
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setBounds(650, 190, 370, 620);
-        this.getContentPane().setBackground(new Color(0xFFF9A1));
+        this.setBounds(650, 190, 560, 620);
+        this.getContentPane().setBackground(new Color(0xFFF8C9));
         this.getContentPane().setLayout(null);
 
         // заполняем комбобокс типами данных PL/SQL
@@ -110,7 +107,7 @@ public class Gui extends JFrame {
 
         // Columns table
         final JScrollPane scrollPane = new JScrollPane();
-        scrollPane.setBounds(10, 255, 336, 283);
+        scrollPane.setBounds(10, 255, 520, 282);
         this.getContentPane().add(scrollPane);
         String[] columns = new String[]{"Header", "Width", "Column", "Type"};
         model = new DefaultTableModel(new Object[69][], columns) {
@@ -133,7 +130,7 @@ public class Gui extends JFrame {
         JTableHeader header = columnsTable.getTableHeader();
         header.setFont(new Font("Tahoma", Font.BOLD, 13));
         //cell alignment
-        DefaultTableCellRenderer Renderer = new DefaultTableCellRenderer();
+        TableColumnsRenderer Renderer = new TableColumnsRenderer();
         Renderer.setHorizontalAlignment(JLabel.CENTER);
         columnsTable.getColumnModel().getColumn(0).setCellRenderer(Renderer);
         columnsTable.getColumnModel().getColumn(1).setCellRenderer(Renderer);
@@ -147,10 +144,10 @@ public class Gui extends JFrame {
         columnsTable.setCellSelectionEnabled(true);
         columnsTable.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
         columnsTable.setFont(new Font("SansSerif", Font.PLAIN, 13));
-        columnsTable.getColumnModel().getColumn(0).setPreferredWidth(80);
-        columnsTable.getColumnModel().getColumn(1).setPreferredWidth(48);
-        columnsTable.getColumnModel().getColumn(2).setPreferredWidth(80);
-        columnsTable.getColumnModel().getColumn(3).setPreferredWidth(110);
+        columnsTable.getColumnModel().getColumn(0).setPreferredWidth(158);
+        columnsTable.getColumnModel().getColumn(1).setPreferredWidth(58);
+        columnsTable.getColumnModel().getColumn(2).setPreferredWidth(166);
+        columnsTable.getColumnModel().getColumn(3).setPreferredWidth(120);
         //colors
         columnsTable.setSelectionBackground(new Color(254, 204, 204));
         scrollPane.setViewportView(columnsTable);
@@ -158,15 +155,26 @@ public class Gui extends JFrame {
         // удаление содержимого ячейки кнопкой Delete
         columnsTable.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
-                if (e.getKeyCode()==127){
-                    columnsTable.setValueAt("", columnsTable.getSelectedRow(), columnsTable.getSelectedColumn());
+                if (e.getKeyCode() == 127){
+                    columnsTable.setValueAt(null, columnsTable.getSelectedRow(), columnsTable.getSelectedColumn());
                 }
             }
         });
 
+        // Open CSV file
+        JButton openCsvFile = new JButton("Open file");
+        openCsvFile.setBounds(10, 547, 120, 22);
+        openCsvFile.setBackground(new Color(203, 221, 251));
+        openCsvFile.setFont(new Font("Tahoma", Font.BOLD, 11));
+        openCsvFile.setContentAreaFilled(true);
+        openCsvFile.setBorderPainted(true);
+        openCsvFile.setFocusable(false);
+        getContentPane().add(openCsvFile);
+        openCsvFile.addActionListener(e -> openFile());
+
         // Create objects button
         JButton setColumnsBtn = new JButton("Create objects");
-        setColumnsBtn.setBounds(224, 547, 120, 22);
+        setColumnsBtn.setBounds(160, 547, 120, 22);
         setColumnsBtn.setBackground(new Color(192, 225, 255));
         setColumnsBtn.setFont(new Font("Tahoma", Font.BOLD, 11));
         setColumnsBtn.setContentAreaFilled(true);
@@ -177,13 +185,19 @@ public class Gui extends JFrame {
 
         // Clear table
         JButton clearTableBtn = new JButton("Clear");
-        clearTableBtn.setBounds(10, 547, 120, 22);
+        clearTableBtn.setBounds(310, 547, 120, 22);
         clearTableBtn.setBackground(new Color(251, 203, 203));
         clearTableBtn.setFont(new Font("Tahoma", Font.BOLD, 11));
         clearTableBtn.setContentAreaFilled(true);
         clearTableBtn.setBorderPainted(true);
         clearTableBtn.setFocusable(false);
         getContentPane().add(clearTableBtn);
+        clearTableBtn.addActionListener((e) -> {
+            for (int i = 0; i < columnsTable.getRowCount(); i++)
+                for(int j = 0; j < columnsTable.getColumnCount(); j++) {
+                    columnsTable.setValueAt(null, i, j);
+                }
+        });
 
         JLabel objectNameLbl = new JLabel("Parameters");
         objectNameLbl.setFont(new Font("Tahoma", Font.BOLD, 13));
@@ -195,17 +209,10 @@ public class Gui extends JFrame {
         columnLbl.setBounds(10, 235, 334, 18);
         getContentPane().add(columnLbl);
 
-        clearTableBtn.addActionListener((e) -> {
-            for (int i = 0; i < columnsTable.getRowCount(); i++)
-                for(int j = 0; j < columnsTable.getColumnCount(); j++) {
-                    columnsTable.setValueAt("", i, j);
-                }
-        });
-
         this.setVisible(true);
     }
 
-
+    // Получение данных из таблиц
     public static void getValues() {
         // определяем количество строк для создания массива (количество столбцов статическое - 4)
         int rowCount = 0;
@@ -241,6 +248,80 @@ public class Gui extends JFrame {
         }
     }
 
+    // Открытие файла для парсинга
+    static void openFile(){
+        JFileChooser open = new JFileChooser();
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("TXT or CSV", "csv", "txt");
+        open.setFileFilter(filter);
+        open.setCurrentDirectory(new File(System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop"));
+        int ret = open.showDialog(null, "Выберите файл");
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            File file = open.getSelectedFile();
+            getLinesFromFile(file.toString());
+        }
+    }
+
+    // Запись кода объектов в файл
+    static void write(String pSql) {
+        //Save file to
+        FileNameExtensionFilter filter = new FileNameExtensionFilter("*.txt", "*.txt", "*.TXT", "*.*");
+        JFileChooser save_to = new JFileChooser();
+        save_to.setFileFilter(filter);
+        save_to.setCurrentDirectory(new File
+                (System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop"));
+        int ret = save_to.showDialog(null, "Save");
+        if (ret == JFileChooser.APPROVE_OPTION) {
+            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(save_to.getSelectedFile() + ".txt"), StandardCharsets.UTF_8)) {
+                writer.write(pSql);
+                writer.flush();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    // Подсчет количества строк в файле
+    static int countLines(String path) {
+        try {
+            LineNumberReader reader = new LineNumberReader(new FileReader(path));
+            int cnt;
+            while (true) {
+                if (reader.readLine() == null) break;
+            }
+            cnt = reader.getLineNumber();
+            reader.close();
+            return cnt;
+        } catch (IOException io) {
+            io.printStackTrace();
+        }
+        return 0;
+    }
+
+    // Считывание всех строк из файла в двумерный массив строк
+    static void getLinesFromFile(String path) {
+        int rowsCount = Gui.countLines(path);
+        String[][] lines = new String[rowsCount][];
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(new FileInputStream(path), StandardCharsets.UTF_8))) {
+            String line;
+            int i = 0;
+            while ((line = reader.readLine()) != null && i < rowsCount) {
+                lines[i++] = line.split(";");
+
+                for (int j = 0; j < 4; j++) {
+                    try {
+                        columnsTable.setValueAt(lines[i - 1][j], i - 1, j);
+                    } catch (IndexOutOfBoundsException e){
+                        columnsTable.setValueAt("-", i - 1, j);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Таблицы с объектами (renderer)
     public static class TableInfoRenderer extends DefaultTableCellRenderer {
         @Override
         public Component getTableCellRendererComponent(JTable table, Object value,
@@ -268,21 +349,40 @@ public class Gui extends JFrame {
         }
     }
 
-    static void write(String pSql) {
-        //Save file to
-        FileNameExtensionFilter filter = new FileNameExtensionFilter("*.txt", "*.txt", "*.TXT", "*.*");
-        JFileChooser save_to = new JFileChooser();
-        save_to.setFileFilter(filter);
-        save_to.setCurrentDirectory(new File
-                (System.getProperty("user.home") + System.getProperty("file.separator") + "Desktop"));
-        int ret = save_to.showDialog(null, "Save");
-        if (ret == JFileChooser.APPROVE_OPTION) {
-            try (OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(save_to.getSelectedFile() + ".txt"), StandardCharsets.UTF_8)) {
-                writer.write(pSql);
-                writer.flush();
-            } catch (IOException e) {
-                e.printStackTrace();
+    // Таблицы со столбцами (renderer)
+    public static class TableColumnsRenderer extends DefaultTableCellRenderer {
+        @Override
+        public Component getTableCellRendererComponent(JTable table, Object value,
+                                                       boolean isSelected, boolean hasFocus, int row, int column) {
+            JLabel c = (JLabel) super.getTableCellRendererComponent(table, value, isSelected, false, row, column);
+
+            if(column == 0) {
+                if (isSelected) {
+                    super.setForeground(new Color(28, 86, 154));
+                } else {
+                    super.setForeground(Color.BLACK);
+                }
+                c.setBackground(new Color(201, 225, 234));
+                c.setHorizontalAlignment(LEFT);
+                c.setFont(new Font("Tahoma", Font.BOLD,13));
+            } else if (column == 1) {
+                if (isSelected) {
+                    super.setForeground(new Color(154, 28, 68));
+                    super.setFont(new Font("Tahoma", Font.BOLD,13));
+                } else {
+                    super.setForeground(Color.BLACK);
+                }
+                c.setBackground(new Color(255, 255, 255));
+                c.setHorizontalAlignment(CENTER);
+            } else {
+                c.setBackground(Color.WHITE);
+                c.setHorizontalAlignment(LEFT);
+                if (isSelected) {
+                    super.setBackground(new Color(254, 204, 204));
+                    super.setFont(new Font("Tahoma", Font.BOLD,13));
+                }
             }
+            return c;
         }
     }
 }
